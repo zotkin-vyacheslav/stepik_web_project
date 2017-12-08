ddc11b3621f2f683278307a8ec968fe617a09146
zotkin-vyacheslav <zotkinslava@gmail.com>
2017-12-08 16:01:54 +0300

    fix'
    Q
    :q
    qqq
    :Q

--- web/init.sh
+++ web/init.sh
@@ -1,2 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
